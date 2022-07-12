4e04abf3dfad50cac765d0306336895a2d1de5eb
ForestLamp <y10b@ya.ru>
2022-07-12 17:18:30 +0300

    Создал TableViewController Clean code Начало архитектуры - MVC

--- VKServices/Supporting Files/AppDelegate.swift
+++ VKServices/Supporting Files/AppDelegate.swift
@@ -7,16 +7,16 @@
 
 import UIKit
 
-// @main
 @UIApplicationMain
 
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var window: UIWindow?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         let window = UIWindow(frame: UIScreen.main.bounds)
-        window.rootViewController = ViewController()
-        window.backgroundColor = .orange
+        let tableViewController = MainTableTableViewController()
+        window.rootViewController = tableViewController
         window.makeKeyAndVisible()
         self.window = window
         return true
